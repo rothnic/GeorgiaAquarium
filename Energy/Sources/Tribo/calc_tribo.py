42ad00471265d35d11bc04926b7d3d4e2870fe44
jcoffeen <jeremy.coffeen@gmail.com>
2014-07-06 19:45:20 -0400

    Update calc_tribo.py
    Updated the calc_cost to include the management tile costs

--- Energy/Sources/Tribo/calc_tribo.py
+++ Energy/Sources/Tribo/calc_tribo.py
@@ -6,11 +6,18 @@ from numba import jit
 
 
 @jit
-def calc_power(tileCount, pedStepsPerTile, triboEff, tribokWh):
-    powerOut = tileCount * pedStepsPerTile * triboEff * tribokWh
+def calc_power(tileCount, pedStepsPerTile, tileEff, tilekWh):
+    powerOut = tileCount * pedStepsPerTile * tileEff * tilekWh
     return powerOut
 
 
 @jit
-def calc_cost(triboUnitCost, tileCount):
-    return triboUnitCost * tileCount
+def calc_cost(tileUnitCost, tileCount, mgtTileUnitCost):
+    if tileCount <= 50
+        return tileUnitCost * tileCount + mgtTileUnitCost
+    else
+        numMgtTiles = int(tileCount / 50)
+        if tileCount mod 50 > 0
+            numMgtTiles = numMgtTiles + 1
+        return (tileUnitCost * tileCount) + (numMgtTiles * mgtTileUnitCost)
+        
