b88d91082d7554ffd2b8de375fee43a4b64cf100
jcoffeen <jeremy.coffeen@gmail.com>
2014-07-21 21:14:08 -0400

    Update calc_tribo.py

--- Energy/Sources/Tribo/calc_tribo.py
+++ Energy/Sources/Tribo/calc_tribo.py
@@ -6,29 +6,24 @@ from numba import jit
 
 
 #@jit
-def calc_power(tileCount, pedStepsPerTile, tileEff, tilePower, timePerStep):
+def calc_power(tileCount, pedStepsPerTile, tileEff, tilePower):
     '''
     Calc_power computes the total number of steps that will occur across all tiles for this given design
     configuration. The total number of steps is affected by the number of tiles and the pedestrian model that
     simulates both on and off season steps per tile.
 
-    .. note:: The vendor advertises watts generated per step, not kWh. So we convert to energy by assuming a step \
-    generates power at the stated power for a fixed period of time, timePerStep
+    .. note:: The vendor does not provide specific power generated per step.  
+    The power generated is assumed to be 1,000,000 steps = 1kWh (1mWh / step) based on a telecon with PaveGen.
+    
 
     :param tileCount: A design variable for how many tiles you would want to purchase
     :param pedStepsPerTile: An uncertainty for the number of expected steps per tile over a year
     :param tileEff: The amount of actual useful energy based on the claimed
     :param tilePower: The claimed power return when one person steps on a tile
-    :param timePerStep: The duration of power generation for one step
     :return: The total energy generated in a year for the input design configuration
     '''
 
-    minutesPerHour = 60.0
-    secondsPerMinute = 60.0
-    timeFactor = (timePerStep/(minutesPerHour * secondsPerMinute))
-
-    return tileCount * pedStepsPerTile * tileEff * tilePower * timePerStep * timeFactor
-
+    return tileCount * pedStepsPerTile * tileEff * tilePower
 
 #@jit
 def calc_cost(tileUnitCost, tileCount, mgtTileUnitCost):
